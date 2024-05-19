166de0b7212d5f4fdd031f45f81106794823f336
Aprita Primayuda <aprita.primayuda@gmail.com>
2024-05-19 22:11:23 +0700

    First draft

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	// configure the paths to all of your source files
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js',
+	],
+	// enable dark mode via class strategy
+  darkMode: 'class',
+	
+	theme: {
+		extend: {
+		// extend base Tailwind CSS utility classes
+
+		},
+	},
+	// add plugins to your Tailwind CSS project
+	plugins: [
+		require('preline/plugin'),
+	],
+}
